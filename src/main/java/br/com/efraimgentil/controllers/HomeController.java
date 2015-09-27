b6e8928c5025ccda4f27a8bbf0117b9cc4177783
root <efraimgentil@gmail.com>
2015-09-27 11:49:35 -0300

    Simple persistence configuration using spring

--- src/main/java/br/com/efraimgentil/controllers/HomeController.java
+++ src/main/java/br/com/efraimgentil/controllers/HomeController.java
@@ -1,13 +1,22 @@
 package br.com.efraimgentil.controllers;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
-
+	
+	@PersistenceContext
+	EntityManager em;
+	
 	@RequestMapping("/hello")
 	public String hello( ) {
+		
+		System.out.println( em );
+		
 		return "hello";
 	}
 
