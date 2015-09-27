9e4edf6f24b7c009ead40e737e3e69513f7756c2
root <efraimgentil@gmail.com>
2015-09-27 11:15:06 -0300

    Hello controller, and a ridiculous bug with cache in eclipse ¬¬

---
+++ src/main/java/br/com/efraimgentil/controllers/HomeController.java
@@ -0,0 +1,14 @@
+package br.com.efraimgentil.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping("/hello")
+	public String hello( ) {
+		return "hello";
+	}
+
+}
